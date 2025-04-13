e55bba9789e6c65aa4c53f138814678700c44fcb
arecouz <richardcouzens96@gmail.com>
2025-04-13 19:33:26 +0100

    admin button

--- frontend/src/pages/LoggedIn.tsx
+++ frontend/src/pages/LoggedIn.tsx
@@ -6,6 +6,12 @@ const LoggedIn = () => {
   const { session, supabaseClient } = useSupabase();
 
   const isAnonymous = session ? session.user?.is_anonymous : false;
+  const isAdmin = session
+    ? session.user?.user_metadata?.role === 'admin'
+    : false;
+
+  console.log(session.user.user_metadata);
+  console.log('is admin????: ', isAdmin);
 
   const handleLogOut = async () => {
     console.log('clicked');
@@ -21,14 +27,14 @@ const LoggedIn = () => {
   };
 
   return (
-    <div className='flex flex-col items-center space-y-4'>
-      <p className='text-lg font-semibold text-gray-800'>
+    <div className="flex flex-col items-center space-y-4">
+      <p className="text-lg font-semibold text-gray-800">
         Logged in as: {session?.user?.email || 'Anonymous User'}
       </p>
-      <div className='flex space-x-4'>
+      <div className="flex space-x-4">
         <button
           onClick={handleLogOut}
-          className='px-6 py-2 text-white bg-red-600 rounded-lg shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500'
+          className="px-6 py-2 text-white bg-red-600 rounded-lg shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500"
         >
           Log Out
         </button>
@@ -36,9 +42,14 @@ const LoggedIn = () => {
         {!isAnonymous && (
           <button
             onClick={addResource}
-            className='px-6 py-2 text-white bg-blue-600 rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500'
+            className="px-6 py-2 text-white bg-blue-600 rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
           >
             Add Resource
+          </button>
+        )}
+        {isAdmin && (
+          <button className="px-6 py-2 text-white bg-gray-700 rounded-lg shadow-md hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-500">
+            ADMIN BUTTON!!!
           </button>
         )}
       </div>
